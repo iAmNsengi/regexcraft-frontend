3cdd86508e82f575adf6b8b62d35d7603b62904e
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-24 18:58:14 +0200

    chore: home initial page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Connecting African Short Films with Global Audiences</h1>
+            <p>Discover, promote, and distribute the best of African short films to local and international buyers.</p>
+            <button>Submit Your Film</button>
+            <button>Explore Films</button>
+        </div>
+    );
+};
+
+export default Home;
+
