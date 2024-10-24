33fc37b748a96295957b1676484cd0ca44e9b325
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-24 19:07:33 +0200

    chore: default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        burntOrange: '#D35400', // Primary Color
+        deepForestGreen: '#0B5345', // Secondary Color
+        richBlack: '#1C1C1C', // Secondary Color
+        goldenYellow: '#F39C12', // Accent Color
+        warmCream: '#F7F4E3', // Accent Color
+      },
+    },
   },
   plugins: [],
 }
