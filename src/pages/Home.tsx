29cbadeb32ddff4a5a5bf8b269bb08772c118791
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-24 19:06:57 +0200

    add: jest&eslint formatter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home: React.FC = () => {
     return (
-        <div>
+        <div className='bg-red-500'>
             <h1>Connecting African Short Films with Global Audiences</h1>
             <p>Discover, promote, and distribute the best of African short films to local and international buyers.</p>
             <button>Submit Your Film</button>
