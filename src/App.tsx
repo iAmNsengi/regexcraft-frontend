d5e2f3a7d97209d22c17d1f2cea889b2152c83df
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-01 13:36:20 +0200

    add: about page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Examples from "./pages/Examples";
 import Playground from "./components/Playground";
 import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="docs" element={<Documentation />} /> */}
+          <Route path="about" element={<About />} />
           <Route path="examples" element={<Examples />} />
           <Route path="playground" element={<Playground />} />
           <Route path="*" element={<NotFound />} />
