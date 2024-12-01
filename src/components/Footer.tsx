9c1e6dce3cd182a88e2e8e63d334defae96bc61a
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-01 13:29:49 +0200

    fix: layout and add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="w-screen bg-gray-800 text-white text-center py-4">
+      Developed with 💖 by{" "}
+      <a href="https://nsengixp.onrender.com/" className="underline">
+        Eliezer Nsengi
+      </a>
+      . ©️ 2024
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-64 bg-gray-50 border-r border-gray-200 p-6">
-        <Navigation />
-      </aside>
-      <main className="flex-1 p-8 overflow-y-auto">
-        <Outlet />
-      </main>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-grow">
+        <aside className="w-64 bg-gray-50 border-r border-gray-200 p-6">
+          <Navigation />
+        </aside>
+        <main className="flex-1 p-8 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 };
