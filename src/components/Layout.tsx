4ff17c4f3e149b750fe9ff89a3cf51462a07a9bb
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-01 13:54:48 +0200

    fix: navigation on small devices

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,11 @@ import Footer from "./Footer";
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden">
-      <div className="flex flex-grow">
-        <aside className="w-64 bg-gray-50 border-r border-gray-200 p-6">
+      <div className="flex flex-grow flex-col md:flex-row">
+        <aside className="w-full md:w-64 bg-gray-50 border-b md:border-r border-gray-200 p-6 md:border-b-0">
           <Navigation />
         </aside>
-        <main className="flex-1 p-8 overflow-y-auto">
+        <main className="flex-1 p-4 md:p-8 overflow-y-auto">
           <Outlet />
         </main>
       </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,40 @@
 // src/components/Navigation/Navigation.tsx
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="flex flex-col space-y-4">
-      <NavLink to="/" className="px-4 py-2 rounded-md hover:bg-gray-200">
+    <nav className="flex items-center overflow-x-scroll md:flex-col md:space-y-4 ">
+      <NavLink
+        to="/"
+        className="px-1 md:px-4 py-2 rounded-md hover:bg-gray-200"
+      >
         Home
       </NavLink>
       <NavLink
         to="/examples"
-        className="px-4 py-2 rounded-md hover:bg-gray-200"
+        className="px-1 md:px-4 py-2 rounded-md hover:bg-gray-200"
       >
         Examples
       </NavLink>
       <NavLink
         to="/playground"
-        className="px-4 py-2 rounded-md hover:bg-gray-200"
+        className="px-1 md:px-4 py-2 rounded-md hover:bg-gray-200"
       >
         Playground
       </NavLink>
 
-      <NavLink to="/about" className="px-4 py-2 rounded-md hover:bg-gray-200">
+      <NavLink
+        to="/about"
+        className="px-1 md:px-4 py-2 rounded-md hover:bg-gray-200"
+      >
         About
       </NavLink>
-      <a
-        href="https://github.com/iAmNsengi/regexcraft"
+      <Link
+        to="https://github.com/iAmNsengi/regexcraft"
         className="px-4 py-2 rounded-md hover:bg-gray-200"
       >
         GitHub
-      </a>
+      </Link>
     </nav>
   );
 };
