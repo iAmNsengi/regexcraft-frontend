0880571d5347d899f5d63658a3f0662a1b138d8e
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-01 18:25:28 +0200

    feat: copy to clipboard

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -13,35 +13,54 @@ const CodeBlock = ({
   language = "typescript",
   showLineNumbers = true,
 }: CodeBlockProps) => {
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(code.trim())
+      .then(() => {
+        alert("Code copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  };
+
   return (
-    <Highlight
-      // theme={themes.themes.nightOwl}
-      code={code.trim()}
-      language={language}
-    >
-      {({ tokens, getLineProps, getTokenProps }) => (
-        <pre className="overflow-x-auto rounded-lg p-4 bg-[#011627]">
-          {tokens.map((line, i) => (
-            <div
-              key={i}
-              {...getLineProps({ line, key: i })}
-              className="table-row"
-            >
-              {showLineNumbers && (
-                <span className="table-cell text-right pr-4 select-none opacity-50 text-white">
-                  {i + 1}
+    <div className="relative">
+      <button
+        onClick={copyToClipboard}
+        className="absolute top-2 right-2 p-1 bg-blue-500 text-white rounded text-xs"
+      >
+        Copy
+      </button>
+      <Highlight
+        // theme={themes.themes.nightOwl}
+        code={code.trim()}
+        language={language}
+      >
+        {({ tokens, getLineProps, getTokenProps }) => (
+          <pre className="overflow-x-auto rounded-lg p-4 bg-[#011627]">
+            {tokens.map((line, i) => (
+              <div
+                key={i}
+                {...getLineProps({ line, key: i })}
+                className="table-row"
+              >
+                {showLineNumbers && (
+                  <span className="table-cell text-right pr-4 select-none opacity-50 text-white">
+                    {i + 1}
+                  </span>
+                )}
+                <span className="table-cell">
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
                 </span>
-              )}
-              <span className="table-cell">
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </span>
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   );
 };
 
