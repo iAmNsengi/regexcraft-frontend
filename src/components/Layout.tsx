3b64bf5caca4bd7f68923665c508ea0b1cac1ad7
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-02 11:02:34 +0200

    fix: layout UI on macbook

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,11 @@ import Footer from "./Footer";
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden">
-      <div className="flex flex-grow flex-col md:flex-row">
-        <aside className="md:fixed md:h-[100vh] w-full md:w-64 bg-gray-200 border-b md:border-r border-gray-200 p-6 md:border-b-0">
+      <div className="flex flex-grow flex-col lg:flex-row  ">
+        <aside className="lg:fixed lg:h-[100vh] w-full lg:w-64 bg-gray-200 border-b md:border-r border-gray-200 p-6 md:border-b-0">
           <Navigation />
         </aside>
-        <main className="flex-1 p-4 md:p-8 overflow-y-auto">
+        <main className="flex-1 lg:ml-[15vw] 2xl:ml-auto  p-4 md:p-8 overflow-y-auto">
           <Outlet />
         </main>
       </div>
