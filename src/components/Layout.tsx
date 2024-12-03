79176f0a4b1df463776613c466f41432f1d65bc8
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-03 08:37:26 +0200

    fix: alignment UI

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,11 @@ import Footer from "./Footer";
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col overflow-x-hidden">
-      <div className="flex flex-grow flex-col lg:flex-row">
+      <div className="flex flex-grow flex-col lg:flex-row ">
         <aside className="w-full border-b border-gray-200 bg-gray-200 p-6 md:border-b-0 md:border-r lg:fixed lg:h-[100vh] lg:w-64">
           <Navigation />
         </aside>
-        <main className="overflow-y-auto lg:ml-64">
+        <main className="overflow-y-auto py-20 lg:mx-auto">
           <Outlet />
         </main>
       </div>
